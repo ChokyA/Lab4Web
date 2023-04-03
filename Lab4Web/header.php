7b84ec95e5893365e85e9f7592090c7b7d942a77
Choky <93463612+ChokyA@users.noreply.github.com>
2023-04-04 04:29:32 +0700

    Add files via upload

---
+++ Lab4Web/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contoh Modularisasi</title>
+    <link href="style.css" rel="stylesheet" type="text/stylesheet"
+media="screen" />
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>DATABASE</h1>
+        </header>
+        <nav>
+            <a href="home.php">Home</a>
+            <a href="tambah.php">Tambah Barang</a>
+        </nav>
